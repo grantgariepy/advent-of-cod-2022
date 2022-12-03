dab93115137e224b08abc9fec3de59c57b7d94da
grantgariepy <grantgariepy@protonmail.com>
2022-12-02 21:04:53 -0700

    second solution

--- day2/day2.py
+++ day2/day2.py
@@ -50,3 +50,46 @@ for game2 in games:
         if((game2[0] == 'C')):
             score2 += 2
 print("Part 1 score:", score1, "Part 2 Score:", score2)
+
+
+# rewrite
+
+
+# with open('day2.txt') as f:
+#     guide = f.read().splitlines()
+
+# score1 = 0
+# score2 = 0
+
+# for round in guide:
+#     match round:
+#         case "A X":
+#             score1 += 1+3
+#             score2 += 3+0
+#         case "A Y":
+#             score1 += 2+6
+#             score2 += 1+3
+#         case "A Z":
+#             score1 += 3+0
+#             score2 += 2+6
+#         case "B X":
+#             score1 += 1+0
+#             score2 += 1+0
+#         case "B Y":
+#             score1 += 2+3
+#             score2 += 2+3
+#         case "B Z":
+#             score1 += 3+6
+#             score2 += 3+6
+#         case "C X":
+#             score1 += 1+6
+#             score2 += 2+0
+#         case "C Y":
+#             score1 += 2+0
+#             score2 += 3+3
+#         case "C Z":
+#             score1 += 3+3
+#             score2 += 1+6
+
+# print(score1)
+# print(score2)
