301b0108d57cc8335906689075ad2fa26f5826f0
grantgariepy <grantgariepy@protonmail.com>
2022-12-04 19:42:55 -0700

    day4 done!

--- day4/day4.ts
+++ day4/day4.ts
@@ -3,7 +3,7 @@ export const x = "";
 const results = await Deno.readTextFile("./day4.txt")
 
 const allAssignments = results.split("\n")
-// console.log(allAssignments)
+
 let count1 = 0
 let count2 = 0
 
@@ -11,13 +11,12 @@ for (const assignment in allAssignments){
   const splitAssignment = allAssignments[assignment].split(',')
   const firstAssignmentSplit = splitAssignment[0].split('-')
   const secondAssignmentSplit = splitAssignment[1].split('-')
-  // console.log(firstAssignmentSplit, secondAssignmentSplit)
+
   // part 1
   if (Number(firstAssignmentSplit[0]) <= Number(secondAssignmentSplit[0]) 
       && Number(firstAssignmentSplit[1]) >= Number(secondAssignmentSplit[1])
       || Number(secondAssignmentSplit[0]) <= Number(firstAssignmentSplit[0])
       && Number(secondAssignmentSplit[1]) >= Number(firstAssignmentSplit[1])){
-    // 
     count1++
   }
 
@@ -26,7 +25,6 @@ for (const assignment in allAssignments){
       && Number(firstAssignmentSplit[1]) >= Number(secondAssignmentSplit[0])
       || Number(secondAssignmentSplit[0]) <= Number(firstAssignmentSplit[0])
       && Number(secondAssignmentSplit[1]) >= Number(firstAssignmentSplit[0])){
-    console.log(firstAssignmentSplit, secondAssignmentSplit)
     count2++
   }
 }
